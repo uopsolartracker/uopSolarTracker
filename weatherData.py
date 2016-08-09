0f302f4b0e2ebaae851cdfc3bbdf2fdafb1d0ed4
Timothy Weatherwax <t_weatherwax@u.pacific.com>
2016-08-08 22:18:21 -0700

    Current and forecasted weather is now captured

--- weatherData.py
+++ weatherData.py
@@ -9,58 +9,75 @@ import json													# Default data from API is in JSON format so need this m
 import datetime												# Used to get a human-readable timestamp
 															# https://docs.python.org/3/library/datetime.html#module-datetime
 from collections import OrderedDict								# Used to order the JSON data from the API in the same order it is imported as. Otherwise, output is unordered
+#import contextlib
 
 ### Request weather data (JSON) from OpenWeatherMap.com
-urlBase = "http://api.openweathermap.org/data/2.5/forecast?q="
-zipCode = "90210"											# University of the Pacific zipcode = 95211
+zipCode = "90210"															# University of the Pacific zipcode = 95211
 APIKey = "849eb6e48e5b5e037e1cb47efea60d62"
-url = urlBase + zipCode + ",us&APPID=" + APIKey					# URL of OpenWeatherMap API
+
+urlForecastBase = "http://api.openweathermap.org/data/2.5/forecast?q="
+forecastURL = urlForecastBase + zipCode + ",us&APPID=" + APIKey					# Forecast URL of OpenWeatherMap API
+
+urlWeatherBase = "http://api.openweathermap.org/data/2.5/weather?q="
+weatherURL = urlWeatherBase + zipCode + ",us&APPID=" + APIKey					# Current weather URL of OpenWeatherMap API
+
+### Read in text file of weather codes into a python dictionary
+idCodeDictionary = {}
+descriptionCodeDictionary = {}
+with open('weatherCodes.txt', 'r') as weatherCodes:
+	skipFirstLine = next(weatherCodes)											# Skip first line of text file
+	for line in weatherCodes:
+		splitLine = line.split()
+		descriptionCodeDictionary[int(splitLine[0])] = ", ".join(splitLine[1:])			# Capture description
+		idCodeDictionary[int(splitLine[0])] = ", ".join(splitLine[1])					# Do not capture description
 
 #example http://api.openweathermap.org/data/2.5/weather?q=65065us&APPID=849eb6e48e5b5e037e1cb47efea60d62
 
-api_response = urllib.request.urlopen(url).read().decode('utf-8')		# Request and read the API page. 
-															# urlopen returns a bytes object so decode it so it can be read by the JSON module
-#TODO: Close the URL with .close()
+def getWeather():
+	### Timestamps
+	time1 = datetime.datetime.utcnow()											# UTC time for timestamp
+	t1 = datetime.datetime.timestamp(time1)									# use datetime to make a timestamp of previous timestamp into a float type
+	californiaTime = datetime.datetime.now()									# Cali time for timestamp
+	#print("TIME = ", californiaTime)
+	
+	### Request and read the API page
+	# urlopen returns a bytes object so decode it so it can be read by the JSON module
+	apiResponseForecast = urllib.request.urlopen(forecastURL).read().decode('utf-8')
+	apiResponseWeather = urllib.request.urlopen(weatherURL).read().decode('utf-8')
+	#TODO: Close the URL with .close()
 
-### Timestamps
-time1 = datetime.datetime.utcnow()								# UTC time for timestamp
-t1 = datetime.datetime.timestamp(time1)						# use datetime to make a timestamp of previous timestamp into a float type
-californiaTime = datetime.datetime.now()						# Cali time for timestamp
-print("TIME = ", californiaTime)
+	### Deserialize the JSON data to a string
+	jsonStringForecast = json.loads(apiResponseForecast, object_pairs_hook=OrderedDict)
+	jsonStringWeather = json.loads(apiResponseWeather, object_pairs_hook=OrderedDict)
 
-### Parse the JSON data
-json_string = json.loads(api_response, object_pairs_hook=OrderedDict)			# Deserialize the data to string
-#print("LOADED = ", json_string, "		")
+	# TODO: make a loop to loop over however many weather ID's there are. we are using about 40 but make it work for any value
+	### Parse the JSON strings for ID and time
+	# Parse the forecast data into a list	
+	forecastID = [ ]
+	forecastTime = [ ]
+	for i in range(30):
+		forecastID.append(jsonStringForecast['list'][i]['weather'][0]['id'])			# Create a list of forecasted weather ID's
+		forecastTime.append(jsonStringForecast['list'][i]['dt'])					# Create a list of forecast time data, API gives time in UTC (unix)
+		#print("i = ", i, " WeatherID = ", weatherID[i])
+		#print("i = ", i, " Time = ", forecastTime[i])	
+	
+	# Parse the current weather data
+	weatherID = jsonStringWeather['weather'][0]['id']
+	weatherTime = jsonStringWeather['dt']
+	print("ID = ", weatherID, " Time = ", weatherTime)
 
-#json_formatted_str = json.dumps(json_string, indent=4)
-#print("Output = ", json_formatted_str)
-##print("Weather ID = ", json_string['list'][0]['weather'][0]['id'])
-# TODO: make a loop to loop over however many weather ID's there are. we are using about 40 but make it work for any value
-weatherID = [ ]
-forecastTime = [ ]
-for i in range(39):
-	weatherID.append(json_string['list'][i]['weather'][0]['id'])			# Create a list of forecasted weather ID's
-	forecastTime.append(json_string['list'][i]['dt'])					# Create a list of forecast time data, API gives time in UTC (unix)
-	print("i = ", i, " WeatherID = ", weatherID[i])
-	print("i = ", i, " Time = ", forecastTime[i])
+	### Check if is raining or might rain
+	if idCodeDictionary[forecastID[0]] == 'x' or idCodeDictionary[weatherID] == 'x':	# If weather in 3 hours or now is raining, close the top of protection unit
+		return 1
+	else:
+		return 0
+	
+######
 
-### Check if is raining or might rain
-# Read in txt file into a python dictionary
-codeDictionary = {}
-with open('weatherCodes.txt', 'r') as weatherCodes:
-	skipFirstLine = next(weatherCodes)
-	for line in weatherCodes:
-		splitLine = line.split()
-		#codeDictionary[int(splitLine[0])] = ", ".join(splitLine[1:])	# Capture description
-		codeDictionary[int(splitLine[0])] = ", ".join(splitLine[1])		# Do not capture description
-if codeDictionary[weatherID[0]] == '-':							# If weather in 3 hours is raining, close the top of protection unit
-	print("win")
-
-###
-
+if __name__ == '__main__':
+	getWeather()
 
 ### Save weather data to a file, save only if had to close top and time and why
 # Keep data for only 1 week?
 
-### Send command to Arduino to close top cover?
 
