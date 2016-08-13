c84565cee4dad3e25a5e4faa53a5d688150283b9
Timothy Weatherwax <t_weatherwax@u.pacific.com>
2016-08-13 12:07:05 -0700

    Main for Pi outlined

---
+++ solarTrackerMain.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import weatherData
+
+### Initialization
+# Set up serial connection with Uno
+# Make sure WIFI is working
+# Connect to server
+# Connect to camera
+# Get inital position of servos?
+
+### Loop
+while(1):
+	### Check weather to see if can open top/raining
+	### Open/close top cover
+	### Check if sun in scope
+		### If not,
+			### Detech edges
+			### Get position of servos on mirror
+			### Calculate how far to move
+			### Send move to Uno
+	### Send good image to server
+
+### Functions
+
+### Interrupts
+#TODO: Make interrupt on time 1hr after nextAPI point when stops raining to allow top cover to open
