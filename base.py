5cc797db3086d4cce90cc4f9c66ed82ac21a05fc
Bercelak <Bercelak@users.noreply.github.com>
2016-09-06 17:41:00 -0700

    Changed base.py: *Solved errors, renamed class to "Base" from "base"

--- base.py
+++ base.py
@@ -2,21 +2,20 @@
 
 import logging
 
-class base:
-	LOG_FILENAME = 'Log_File.txt'
-	FORMAT = '%(asctime)s -- %(module)s:%(funcName)s -- %(message)s'
-	DATEFORMAT = '%Y-%m-%d %H:%M:%S'
+class Base:
+    FORMAT = '%(asctime)s -- %(module)s:%(funcName)s -- %(message)s'
+    DATEFORMAT = '%Y-%m-%d %H:%M:%S'
 
-	def __init__(self):
-		self.Log = logging.getLogger('__name__')
-		Log.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG,datefmt=DATEFORMAT,format=FORMAT)
+    def __init__(self, log_filename):
+        self.Log = logging.getLogger('__name__')
+        self.Log.basicConfig(filename=self.log_filename, level=logging.DEBUG, datefmt=self.DATEFORMAT, format=self.FORMAT)
 
-# Level		Numeric value
-# CRITICAL		50
-# ERROR			40
-# WARNING		30
-# INFO			20
-# DEBUG			10
-# NOTSET		0
-	def LogM(self,message,level):
-		Log.log(level, message)
+	# Level		Numeric value
+	# CRITICAL		50
+	# ERROR			40
+	# WARNING		30
+	# INFO			20
+	# DEBUG			10
+	# NOTSET		0
+	def LogM(self, message, level):
+		self.Log.log(level, message)
