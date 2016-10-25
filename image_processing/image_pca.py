490abeb0db0fea8a05a50cc2bfd9886bb7b59688
Daniel Herink <engineertandcode@gmail.com>
2016-10-25 16:47:25 -0700

    Image Processing: Fixing region properties analysis: --The center, xc and yc, was calculated incorrectly
    The picture is probably throwing off the PCA functions. Going to edit the image to remove extraneous parts

--- image_processing/image_pca.py
+++ image_processing/image_pca.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
 	ret,threshImg = cv2.threshold(grayImg, 127, 255, cv2.THRESH_BINARY)
 
 	kernel = numpy.uint8([[0, 1, 0],[1, 1, 1],[0, 1, 0]])
-	eroded = cv2.erode(threshImg, kernel, iterations = 2)
-	dilated = cv2.erode(eroded, kernel, iterations = 2)
+	eroded = cv2.erode(threshImg, kernel, iterations = 5)
+	dilated = cv2.erode(eroded, kernel, iterations = 5)
 
 	mu00, mu11, mu02, mu20, xc, yc = RegionProperties(dilated, 0)
 	eig_vals, theta = PCA(mu00, mu11, mu02, mu20)
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 	pyplot.imshow(eroded, cmap = 'gray')
 	pyplot.figure("Dilated Image")
 	pyplot.imshow(dilated, cmap = 'gray')
-	pyplot.figure("Original With PCA")
+	pyplot.figure("Original With Center (" + str(xc) + "," + str(yc) + ")")
 	pyplot.imshow(grayImg, cmap = 'gray')
 	pyplot.plot(line1[0],line1[1], 'k-')
 	pyplot.plot(line2[0],line2[1], 'k-')
--- image_processing/region_properties.py
+++ image_processing/region_properties.py
@@ -28,26 +28,24 @@ def RegionProperties(Input,label):
                 m20 = m20 + x*x
                 m02 = m02 + y*y
                 area = area + 1
-        xc = m10/m00
-        yc = m01/m00
+    xc = m10/m00
+    yc = m01/m00
     for x in range(1,height):
         for y in range(1,width):
             if(Input[x,y]==label):
                 mu01 = mu01 + (y - yc)
                 mu10 = mu10 + (x - xc)
                 mu11 = mu11 + (x - xc)*(y - yc)
-                mu20 = mu20 + (x - xc)**2
-                mu02 = mu02 + (y - yc)**2
+                mu20 = mu20 + math.pow((x - xc),2)
+                mu02 = mu02 + math.pow((y - yc),2)
     mu00 = m00
 
     return mu00, mu11, mu02, mu20, xc, yc
 
 def PCA(mu00, mu11, mu02, mu20):
-    c = numpy.array([[mu20, mu11],
-    [mu11, mu02]]).astype(float)*1/mu00
     eig_vals = [0,0]
-    eig_vals[0] = 1/(2*mu00)*(mu20 + mu02 + math.sqrt((mu20-mu02)**2 + 4*mu11**2))
-    eig_vals[1] = 1/(2*mu00)*(mu20 + mu02 - math.sqrt((mu20-mu02)**2 + 4*mu11**2))
+    eig_vals[0] = 1/(2*mu00)*(mu20 + mu02 + math.sqrt(math.pow((mu20-mu02),2) + math.pow(4*mu11,2)))
+    eig_vals[1] = 1/(2*mu00)*(mu20 + mu02 - math.sqrt(math.pow((mu20-mu02),2) + math.pow(4*mu11,2)))
     theta = 0.5*math.atan2(2*mu11,mu20-mu02)
 
     return eig_vals, theta
