85245414b42a458441b8fa2202aaea8e111dcb0e
Daniel Herink <engineertandcode@gmail.com>
2016-11-01 06:23:00 -0700

    Image Processing: Error found - Human error was to blame; the wrong label was passed to the PCA function, and instead of looking for white areas it was looking for black areas, thus throwing off the calculations
    Fixed and PCA now working properly - Image processing functionality complete
    
    TODO: Integrate with main program

--- image_processing/image_pca.py
+++ image_processing/image_pca.py
@@ -11,13 +11,13 @@ if __name__ == '__main__':
 	# TODO: Add debug output if file does not load properly
 	grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	ret,threshImg = cv2.threshold(grayImg, 90, 255, cv2.THRESH_BINARY)
-	cv2.imwrite('sun_thresh.bmp',threshImg)
+	# cv2.imwrite('sun_thresh.bmp',threshImg)
 
 	# kernel = numpy.uint8([[0, 1, 0],[1, 1, 1],[0, 1, 0]])
 	# eroded = cv2.erode(threshImg, kernel, iterations = 2)
 	# dilated = cv2.erode(eroded, kernel, iterations = 2)
 
-	mu00, mu11, mu02, mu20, xc, yc = RegionProperties(threshImg,0)#dilated, 0)
+	mu00, mu11, mu02, mu20, xc, yc = RegionProperties(threshImg,255)#dilated, 0)
 	eig_vals, theta = PCA(mu00, mu11, mu02, mu20)
 	line1, line2, line3, line4 = GetAxes(xc, yc, theta, eig_vals)
 
