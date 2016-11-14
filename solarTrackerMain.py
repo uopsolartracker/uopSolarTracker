61efc85d31c9a3e684764d1cf8c8f2001378ee72
jrodri29 <j_rodriguez19@u.pacific.edu>
2016-11-13 21:23:35 -0800

    serial comm. added with servo movement

--- solarTrackerMain.py
+++ solarTrackerMain.py
@@ -1,19 +1,44 @@
 #!/usr/bin/python3
 
-import weatherData
+import serial
+from base import*
+from weatherData import *
+from sun_position_calc import *
+from region_properties import*
+from image_pca import*
+from servo_angle_change import *
+
 
 ### Initialization
-# Start logging feature
-# Set up serial connection with Uno
-# Make sure WIFI is working
-# Connect to server
-# Connect to camera
-# Get inital position of servos?
+#------ Start logging feature-------
+
+#----- Set up serial connection with Uno-------              
+# Turn the Serial Protocol ON
+port = '/dev/ttyACM0'
+baud = 9600
+Serial.begin(9600);
+ser = serial.Serial(port, baud, timeout = 6)
+
+#----- Make sure WIFI is working-------
+
+#------Connect to server-------
+#------Connect to camera-------
+
+#-----Get inital position of servos------
+val_vertical=str(y_zenith[0]);
+val_horizontal=str(x_azimuth[0]);
 
 ### Loop
 while(1):
 	### Check weather to see if can open top/raining
+	getWeatherForTop()
+	getForecastForTop()
+	
 	### Open/close top cover
+	### Move servo
+	ser.write(val_vertical.encode('utf-8'))
+	ser.write(val_vertical.encode('utf-8'))
+	time.sleep(3)
 	### Check if sun in scope
 		### If not,
 			### Detech edges
