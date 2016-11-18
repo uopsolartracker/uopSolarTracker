821c4ef0b9d28d4d63ad0c68f3820fa600abfd8a
jrodri29 <j_rodriguez19@u.pacific.edu>
2016-11-17 20:21:26 -0800

    Added functions to call from for mainSolarTracker

--- servo_position_change.py
+++ servo_position_change.py
@@ -9,81 +9,113 @@ import cmath
 import sched  
 
 #----inputs for testing proposes-----
-old_X= 100;
-old_Y= 567;
-imageCenter_x=640;
-imageCenter_y=480;
-sunCenter_x=696;
-sunCenter_y=558;
+old_j= 100;
+old_i= 567;
+
+#----image size---
+imageWidth=imageCenter_i *2;
+imageHeight=imageCenter_j *2;
 
 #---Compute left, right, up, down pixel amounts from image center---
-## left and right
-if imageCenter_x > sunCenter_x:
-	rightPixel= imageCenter_x-sunCenter_x;
-else:
-	rightPixel=0;
-if imageCenter_x < sunCenter_x:
-	leftPixel=	sunCenter_x-imageCenter_x;
-else:
-	leftPixel=0;
 
-## up and down
-if imageCenter_y > sunCenter_y:
-	upPixel= imageCenter_y-sunCenter_y;
-else:
-	upPixel=0;
-if imageCenter_y < sunCenter_y:
-	downPixel=	sunCenter_y-imageCenter_y;
-else:
-	downPixel=0;	
+def pixel_distance(imageCenter_i, imageCenter_j, sunCenter_i, sunCenter_j):
 	
-#----image size---
-imageWidth=imageCenter_x *2;
-imageHeight=imageCenter_y *2;
+	## left and right
+	if imageCenter_j > sunCenter_j:
+		rightPixel= imageCenter_j-sunCenter_j;
+	else:
+		rightPixel=0;
+	if imageCenter_j < sunCenter_j:
+		leftPixel=	sunCenter_j-imageCenter_j;
+	else:
+		leftPixel=0;
 
-#----Servo Angle Movemnet----
-servoX=47;
-servoY=38;
+	## up and down
+	if imageCenter_i > sunCenter_i:
+		upPixel= imageCenter_i-sunCenter_i;
+	else:
+		upPixel=0;
+	if imageCenter_i < sunCenter_i:
+		downPixel=	sunCenter_i-imageCenter_i;
+	else:
+		downPixel=0;
+	return (rightPixel, leftPixel, downPixel, upPixel)		
 
-#----pixel per degree----
-horizontal=imageWidth/servoX;
-vertical=imageWidth/servoY;
 
-#******New Position Calculation******
+def acceptedErrorCheck(rightPixel, leftPixel, downPixel, upPixel):
+	flag1=0;
+	flag2=0;
+	if (rightPixel > 100)  or (leftPixel > 100):
+		flag1= 1;
+	else:
+		flag1=0;
+	if (downPixel > 100 ) or (upPixel > 100):
+		flag2=1;
+	else: 
+		flag2=0;
+	return (flag1, flag2)	
 
-##----Azimuth Angle Change----
-if rightPixel != 0:
-	xChange= rightPixel/horizontal;
-if leftPixel != 0:
-	xChange=-(leftPixel/horizontal);
+def SunCenteredCheck(flag1, flag2):
+	iChange=0;
+	jChange=0;
+	if (flag1 == 1 or flag2 ==1):
+		move=1;
+		#----Servo Angle Movemnet----
+		servo_j=47;
+		servo_i=38;
+
+		#----pixel per degree----
+		horizontal=imageWidth/servo_j;
+		vertical=imageHeight/servo_i;
 		
-##----Zenith Angle Change-----
-if upPixel != 0:
-	yChange= -(upPixel/vertical);	
-if downPixel != 0:
-	yChange=downPixel/vertical;
-
-
-##------normalize angles and truncate to 4 decimal places-----
-x_angle = 0.3125*round(xChange/0.3125)
-y_angle = 0.3125*round(yChange/0.3125)
+		##----Azimuth Angle Change----
+		if rightPixel != 0:
+			jChange= rightPixel/horizontal;
+		if leftPixel != 0:
+			jChange=-(leftPixel/horizontal);
+				
+		##----Zenith Angle Change-----
+		if upPixel != 0:
+			iChange= -(upPixel/vertical);	
+		if downPixel != 0:
+			iChange=downPixel/vertical;
+	else:
+		move=0;	
+	return(move, iChange, jChange)
 
 def truncate (f, n):
- 	s = '%.12f' % f
- 	i, p, d = s.partition('.')
- 	return ('.'.join([i, (d+'0'*n)[:n]]));
+	s = '%.12f' % f
+	i, p, d = s.partition('.')
+	return ('.'.join([i, (d+'0'*n)[:n]]));
+			
+def sendPosition(move, iChange, jChange):
+	motor_i=0;
+	motor_j=0;
+	if move == 1:
+		##------normalize angles and truncate to 4 decimal places-----
+		j_angle = 0.3125*round(jChange/0.3125)
+		i_angle = 0.3125*round(iChange/0.3125)
+			
+		j_angle=truncate(j_angle, 4)
+		i_angle=truncate(i_angle, 4)
+
+		##-----New position angle-----
+		old_j=old_j*0.3125;
+		old_i=old_i*0.3125;
+
+		new_j= old_j+float(j_angle);
+		new_i= old_i+float(i_angle);
+
+		##---- motor command-------
+		motor_j=int(new_j/0.3125);
+		motor_i=int(new_i/0.3125);
+	else:
+		move=0	
+	return(move, motor_i, motor_j)
+
+
+[rightPixel, leftPixel, downPixel, upPixel]=pixel_distance(imageCenter_i, imageCenter_j, sunCenter_i, sunCenter_j)	
+[flag1, flag2]=acceptedErrorCheck(rightPixel, leftPixel, downPixel, upPixel)
+[move, iChange, jChange]=SunCenteredCheck(flag1, flag2)
+sendPosition(move, iChange, jChange)	
 	
-x_angle=truncate(x_angle, 4)
-y_angle=truncate(y_angle, 4)
-
-##-----New position angle-----
-old_X=old_X*0.3125;
-old_Y=old_Y*0.3125;
-
-new_X= old_X+float(x_angle);
-new_Y= old_Y+float(y_angle);
-
-##---- motor command-------
-motor_X=int(new_X/0.3125);
-motor_Y=int(new_Y/0.3125);
-
